9138f9d9cd445eb98c7efafda9de0bddc26d6bf9
ValentinaVarela <varelavalen931@gmail.com>
2022-07-21 17:43:14 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//CREANDO VARIABLES
+
+let nombreUsuario="juan jose"
+const EDAD_USUARIO=33
+let estaturaUsuario=1.62
+let hinchaUsuario=true
+
+console.log(nombreUsuario)
+
+EDAD_USUARIO=20
+console.log(EDAD_USUARIO)
