3e882025c59bfae81005883c9b7d40dd5108adad
407 <407@academicos.cesde.edu.co>
2022-08-19 12:59:01 -0500

    se agrega nuevos cambios

--- src/controllers/aprendiendoDOM.js
+++ src/controllers/aprendiendoDOM.js
@@ -10,3 +10,15 @@ etiquetaTitulo.textContent="jefe como estas"
 
 //1.2 MANIPULANDO EL SRC
 etiquetaImagen.src="http://definicionde.hugex.net/wp-content/uploads/2015/07/e1001e86903d5fccba2a7e83a0547bd4.jpg"
+
+//2. CONTROLANDO EL DISEÑO 
+let parrafo=document.getElementById("parrafo")
+parrafo.textContent="“Qué buen insomnio si me desvelo sobre tu cuerpo”"
+
+//2.1 AGREGANDO UN ESTILO (CLASS)
+//text_danger
+parrafo.classList.add("text-danger", "fs-1", "text-center")
+etiquetaImagen.classList.add("d-block", "mx-auto")
+
+//2.2 QUITANDO UN ESTILO
+parrafo.classList.remove("text-danger")
--- src/controllers/detalleProducto.js
+++ src/controllers/detalleProducto.js
@@ -4,6 +4,18 @@ console.log("hola estoy detallando la compra")
 let producto=JSON.parse(localStorage.getItem("producto"))
 console.log(producto)
 
+let carrito
+if(JSON.parse(localStorage.getItem("carrito"))!=null){
+    carrito=JSON.parse(localStorage.getItem("carrito"))
+    let pildora=document.getElementById("pildora")
+    pildora.textContent=carrito.length
+}else{
+    carrito=[]
+}
+
+let carritoMemoria=JSON.parse(localStorage.getItem("carrito"))
+console.log(carritoMemoria)
+
 //referenciar la imagen del producto
 let foto=document.getElementById("imagenInfo")
 foto.src=producto.foto
@@ -12,7 +24,34 @@ let nombre=document.getElementById("nombreInfo")
 nombre.textContent=producto.nombre
 
 let precio=document.getElementById("precioInfo")
-precio.textContent.producto.precio
+precio.textContent=producto.precio
 
 let descripcion=document.getElementById("descripcionInfo")
 descripcion.textContent=producto.descripcion
+
+let pildora=document.getElementById("pildora")
+console.log(pildora.textContent)
+
+//escucho el click en el boton añadir al carrito
+let botoncarrito=document.getElementById("botonAgregarCarrito")
+botoncarrito.addEventListener("click",function(evento){
+
+    let cantidad=document.getElementById("cantidad")
+    cantidad=cantidad.value
+    console.log("agregaste",cantidad)
+
+    //agregamos la cantidad al objeto producto
+    producto.cantidad=cantidad
+    console.log(producto)
+
+    //agregamos el producto al carrito de compras
+    carrito.push(producto)
+    console.log(carrito)
+
+    //agregando el carrito a la memoria
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+
+    //pintando la pildora con la cantidad de productos
+    let cantidadCarrito=carrito.length
+    pildora.textContent=cantidadCarrito
+})
